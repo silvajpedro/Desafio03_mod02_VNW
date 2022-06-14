2415ec963e43ca04ff964a972677f8023c8e215f
silvajpedro <belojpedro.31@gmail.com>
2022-06-14 08:36:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("----------------")
+console.log("Primeiro exercício de 1 até 10")
+
+for(let i = 1; i <= 10; ++i){
+    console.log(i)
+}
+console.log("----------------")
+console.log("Segundo exercício de 10 até 1")
+let a = 10
+while(a > 0 ){
+    console.log(a)
+    --a
+}
+console.log("----------------")
+console.log("Terceiro exercício, que conte todos os números ímpares de 1 até 100")
+for(let i = 1; i <= 100; i+=2){
+    console.log(i)
+}
+console.log("----------------")
+console.log("Quarto exercício, que conte todos os números pares de 0 até 100")
+for( let i = 0; i <= 100; i+=2){
+    console.log(i)
+}
+
